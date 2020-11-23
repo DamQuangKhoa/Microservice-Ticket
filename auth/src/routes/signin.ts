839d8b7464ce6d6441e96b89d0a5f64a1893e759
Ted <dqkhoa02@gmail.com>
2020-11-23 22:54:33 +0700

    [DevOps][K8S][NodeJS] Add Sign In Flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,10 @@
 import express, {Request, Response} from 'express'
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import jwt from 'jsonwebtoken'
+import { body} from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
 import { vallidateRequest } from '../middlewaves/validate-request';
-
+import { User } from '../models/user';
+import { Password } from '../services/password';
 const router = express.Router();
 
 router.post('/api/users/sign-in',[
@@ -13,9 +15,27 @@ router.post('/api/users/sign-in',[
     .trim()
     .notEmpty()
     .withMessage('You must supply a password')
-], vallidateRequest, (req: Request , res: Response) => {
+], vallidateRequest, async (req: Request , res: Response) => {
     const { email, password } = req.body
-    res.send('Hi There')
+    const existingUser = await User.findOne(({email}));
+    if(!existingUser){
+        throw new BadRequestError('Invalid Credentials')
+    }
+    const passwordMatch = await Password.compare(existingUser.password, password)
+    if(!passwordMatch){
+        throw new BadRequestError('Invalid Credentials')
+    }
+     // Generate JWT Token
+    const userJWT = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+    }, process.env.JWT_KEY! )
+    // Store it on session object
+    req.session = {
+        jwt: userJWT
+    }
+
+    res.status(201).send(existingUser)
 })
 
 export {router as signInRouter};
