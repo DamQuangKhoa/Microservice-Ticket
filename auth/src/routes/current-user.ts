7b11c56e402c0a57ef447bf0a088f1bc438b8fac
Ted <dqkhoa02@gmail.com>
2020-11-24 11:33:35 +0700

    [DevOps][K8S][NodeJS] Add Required Authen Middlewave

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import { currentUser } from '../middlewaves/current-user';
+import { requireAuth } from '../middlewaves/require-auth';
 
 
 const router = express.Router();
 
-router.get('/api/users/current-user', currentUser , (req, res) => {
+router.get('/api/users/current-user', currentUser, requireAuth, (req, res) => {
     res.send({currentUser: req.currentUser || null })
 })
 
