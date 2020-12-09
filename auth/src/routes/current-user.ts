9a5fd21f38b9fca38e4f931d2cdbf3489843e125
Ted <dqkhoa02@gmail.com>
2020-12-09 15:08:35 +0700

    [DevOps][K8S][NextJS] Add Client Service

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,11 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import { currentUser } from '../middlewaves/current-user';
-import { requireAuth } from '../middlewaves/require-auth';
 
 
 const router = express.Router();
 
-router.get('/api/users/current-user', currentUser, requireAuth, (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
     res.send({currentUser: req.currentUser || null })
 })
 
