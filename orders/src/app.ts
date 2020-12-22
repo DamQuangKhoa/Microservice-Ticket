a462033c442a0a3147ebe6dd6ae63c9e5ddf529c
Ted <dqkhoa02@gmail.com>
2020-12-22 17:03:39 +0700

    [DevOps][K8S][NodeJS] Add Order Service

---
+++ orders/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { json } from 'body-parser';
+import 'express-async-errors';
+import cookieSession from 'cookie-session'
+import { errorHandler, NotFoundError, currentUser } from '@tedvntickets/common';
+
+const app = express();
+app.set('trust proxy', true)
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test' // if test > false
+    })
+); 
+app.use(currentUser);
+
+app.all('*', async () => {
+    throw new NotFoundError()
+})
+app.use(errorHandler)
+
+export { app };
