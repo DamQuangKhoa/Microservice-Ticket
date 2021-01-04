eb5bfd18586674b04e4769f33646df4c7656ac79
Ted <dqkhoa02@gmail.com>
2021-01-04 14:29:06 +0700

    [DevOps][K8S][NodeJS] Add Order Service Test

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,23 +1,34 @@
 import express from 'express';
-import { json } from 'body-parser';
 import 'express-async-errors';
-import cookieSession from 'cookie-session'
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@tedvntickets/common';
 
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+
 const app = express();
-app.set('trust proxy', true)
+app.set('trust proxy', true);
 app.use(json());
 app.use(
-    cookieSession({
-        signed: false,
-        secure: process.env.NODE_ENV !== 'test' // if test > false
-    })
-); 
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
 app.use(currentUser);
 
-app.all('*', async () => {
-    throw new NotFoundError()
-})
-app.use(errorHandler)
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 export { app };
