ed9f20ea21705f441691fe563b792f038a8a750d
Ted <dqkhoa02@gmail.com>
2021-02-12 22:44:30 +0700

    [DevOps][K8S][NodeJS] Update Auth Service

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -5,7 +5,7 @@ import { app } from '../../app';
 it('responds with detail about user', async () => {
     const cookie = await global.signin()
     const response = await request(app)
-            .get('/api/users/current-user')
+            .get('/api/users/currentuser')
             .set('Cookie', cookie)
             .send()
             .expect(200)
